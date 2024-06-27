8e2a2142d19978234c2a887c636556c21a892a8d
ssenpgowtham <122586640+ssenpgowtham@users.noreply.github.com>
2024-06-27 10:29:08 +0530

    Add files via upload

---
+++ prog26.c
@@ -0,0 +1,9 @@
+#include<stdio.h> 
+int main()
+{
+int a,b,c; /*varible declaration*/ 
+printf(“enter two numbers \n”);
+ scanf(“%d %d”,&a,&b); c=a+b;//adding two numbers
+ printf(“sum is %d \n”,c);
+return 0;
+}
